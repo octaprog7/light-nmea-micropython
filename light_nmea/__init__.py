ef0abd9368ed48f817df71261a7f8a17a45fb6cc
roman <kolbasilyvasily@yandex.ru>
2026-07-03 01:41:57 +0500

    - license. + __version__.

--- light_nmea/__init__.py
+++ light_nmea/__init__.py
@@ -1,14 +1,8 @@
-# Copyright 2026 Roman Shevchik
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Copyright 2026 Roman Shevchik. See LICENSE for details.
+"""LightNMEA - Fast NMEA-0183 parser.
+Usage:
+    from light_nmea.nmea0183_parser import LightNMEA
+    from light_nmea.nmea0183_stream import NMEAStreamReader
+    from light_nmea.conv_to_hrf import to_format
+"""
+__version__ = "1.0.1"
