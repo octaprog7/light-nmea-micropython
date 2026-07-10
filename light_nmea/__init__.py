ce92a3af6f282ac0f56a8e8b4fedf51627ace173
roman <kolbasilyvasily@yandex.ru>
2026-07-10 23:13:14 +0500

    fix: Добавил экспорт компонент для удобного доступа.

--- light_nmea/__init__.py
+++ light_nmea/__init__.py
@@ -8,3 +8,8 @@ Usage:
 __version__ = "2.0.0"
 __author__ = "Roman Shevchik"
 __license__ = "GPL-3.0"
+
+# Экспорт компонент для удобного доступа
+from light_nmea.nmea0183_parser import LightNMEA
+from light_nmea.nmea0183_stream import NMEAStreamReader
+from light_nmea.conv_to_hrf import to_format
