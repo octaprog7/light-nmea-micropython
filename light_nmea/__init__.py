605dde6b32b49ab315a3e126bb88bd31c26fff3f
roman <kolbasilyvasily@yandex.ru>
2026-07-11 16:29:40 +0500

    fix: исправил код импорта на относительный.

--- light_nmea/__init__.py
+++ light_nmea/__init__.py
@@ -1,15 +1,63 @@
 # Copyright 2026 Roman Shevchik. See LICENSE for details.
-"""LightNMEA - Fast NMEA-0183 parser.
-Usage:
-    from light_nmea.nmea0183_parser import LightNMEA
-    from light_nmea.nmea0183_stream import NMEAStreamReader
-    from light_nmea.conv_to_hrf import to_format
-"""
-__version__ = "2.0.0"
-__author__ = "Roman Shevchik"
-__license__ = "GPL-3.0"
 
-# Экспорт компонент для удобного доступа
-from light_nmea.nmea0183_parser import LightNMEA
-from light_nmea.nmea0183_stream import NMEAStreamReader
-from light_nmea.conv_to_hrf import to_format
+# Импортирую ядро парсера и ВСЕ его публичные постоянные
+from .nmea0183_parser import (
+    LightNMEA,
+    # Маски созвездий для фильтрации
+    CST_MASK_ALL,
+    CST_MASK_GPS,
+    CST_MASK_GLONASS,
+    CST_MASK_GALILEO,
+    CST_MASK_BEIDOU,
+    CST_MASK_QZSS,
+    CST_MASK_NAVIC,
+    # Идентификаторы созвездий (Talker ID)
+    CST_UNKNOWN,
+    CST_GPS,
+    CST_GLONASS,
+    CST_GALILEO,
+    CST_BEIDOU,
+    CST_QZSS,
+    CST_NAVIC,
+    CST_MULTI,
+    # Статусы качества фикса (Fix Quality)
+    FIX_NOT_VALID,
+    FIX_AUTONOMOUS,
+    FIX_DGPS,
+    FIX_RTK_FIXED,
+    FIX_RTK_FLOAT,
+    FIX_ESTIMATED,
+    # Константы режимов сброса состояния (RESET)
+    RESET_ALL,
+    RESET_GGA,
+    RESET_RMC
+)
+
+# Импортирую стрим-ридер с правильным именем класса
+from .nmea0183_stream import NMEAStreamReader
+
+# Импортирую функции конвертации
+from .conv_to_hrf import to_format
+
+# Публичный API пакета через __all__
+__all__ = [
+    # Основные классы и функции
+    'LightNMEA',
+    'NMEAStreamReader',
+    'to_format',
+
+    # Константы созвездий
+    'CST_UNKNOWN', 'CST_GPS', 'CST_GLONASS', 'CST_GALILEO',
+    'CST_BEIDOU', 'CST_QZSS', 'CST_NAVIC', 'CST_MULTI',
+
+    # Маски для фильтра
+    'CST_MASK_ALL', 'CST_MASK_GPS', 'CST_MASK_GLONASS',
+    'CST_MASK_GALILEO', 'CST_MASK_BEIDOU', 'CST_MASK_QZSS', 'CST_MASK_NAVIC',
+
+    # Статусы навигации
+    'FIX_NOT_VALID', 'FIX_AUTONOMOUS', 'FIX_DGPS',
+    'FIX_RTK_FIXED', 'FIX_RTK_FLOAT', 'FIX_ESTIMATED',
+
+    # Режимы сброса
+    'RESET_ALL', 'RESET_GGA', 'RESET_RMC'
+]
