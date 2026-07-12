739d977d759843633dfcef8ae158ed72d02d88c5
roman <kolbasilyvasily@yandex.ru>
2026-07-12 13:45:46 +0500

    fix: добавил 'FMT_TXT', 'FMT_CSV', 'FMT_JSON', 'FMT_COMPACT'.

--- light_nmea/__init__.py
+++ light_nmea/__init__.py
@@ -33,12 +33,13 @@ from light_nmea.nmea0183_parser import (
 from light_nmea.nmea0183_stream import NMEAStreamReader
 
 # Абсолютный импорт утилит преобразования
-from light_nmea.conv_to_hrf import to_format
+from light_nmea.conv_to_hrf import to_format, FMT_TXT, FMT_CSV, FMT_JSON, FMT_COMPACT
 
 __all__ = [
     'LightNMEA',
     'NMEAStreamReader',
     'to_format',
+    'FMT_TXT', 'FMT_CSV', 'FMT_JSON', 'FMT_COMPACT',
     'CST_UNKNOWN', 'CST_GPS', 'CST_GLONASS', 'CST_GALILEO',
     'CST_BEIDOU', 'CST_QZSS', 'CST_NAVIC', 'CST_MULTI',
     'CST_MASK_ALL', 'CST_MASK_GPS', 'CST_MASK_GLONASS',
